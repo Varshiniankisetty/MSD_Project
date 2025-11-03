ccb8c29948592764502f9e3fb062d504e873a184
Varshiniankisetty <ankisettyvarshini21@gmail.com>
2025-11-03 14:03:54 +0530

    Update API URL to production endpoint

--- SCAMT/src/api/auth.js
+++ SCAMT/src/api/auth.js
@@ -1,7 +1,7 @@
 // src/api/auth.js
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://msd-project-backend.onrender.com/api/auth";
 
 export const registerUser = async (formData) => {
   const res = await axios.post(`${API_URL}/register`, formData);
